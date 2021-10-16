5703331d7fd78ca038fe4202cd9d4228edf62745
Lucky Wirasakti <lucky.wirasakti@gmail.com>
2021-10-16 15:38:02 +0700

    remove cache

--- app/config.py
+++ app/config.py
@@ -2,5 +2,3 @@ import os
 
 GOOGLE_CHROME_PATH = os.environ.get('GOOGLE_CHROME_SHIM', None)
 CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', 'chromedriver')
-KEY_CACHE = os.environ.get('KEY_CACHE', 'lucky')
-TTL_CACHE = os.environ.get('TTL_CACHE', 300)
--- app/main.py
+++ app/main.py
@@ -1,19 +1,15 @@
 from json import loads
 
 from flask import Flask, jsonify, request
-from flask_cacheify import init_cacheify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from app.config import (
-    CHROMEDRIVER_PATH, 
-    GOOGLE_CHROME_PATH, 
-    KEY_CACHE,
-    TTL_CACHE
+    CHROMEDRIVER_PATH,
+    GOOGLE_CHROME_PATH
 )
 
 app = Flask(__name__)
-cache = init_cacheify(app)
 
 
 @app.route('/')
@@ -23,22 +19,25 @@ def root():
 
 @app.route('/scrapper')
 def handler():
+    options = webdriver.ChromeOptions()
+    options.binary_location = GOOGLE_CHROME_PATH
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument(
+        'user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/84.0.4147.125 Safari/537.36'
+    )
+    wb = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH,
+        options=options
+    )
     try:
-        return cache.get(KEY_CACHE)
-    except:
-        options = webdriver.ChromeOptions()
-        options.binary_location = GOOGLE_CHROME_PATH
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument(
-            'user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/84.0.4147.125 Safari/537.36'
+        wb.get(request.args.get('url'))
+        return jsonify(
+            loads(
+                loads(
+                    wb.find_element(by=By.TAG_NAME, value="pre").text
+                )
+            )
         )
-        wb = webdriver.Chrome(
-            executable_path=CHROMEDRIVER_PATH, options=options)
-        try:
-            wb.get(request.args.get('url'))
-            cache.set(KEY_CACHE, jsonify(
-                loads(loads(wb.find_element(by=By.TAG_NAME, value="pre").text))), TTL_CACHE)
-            return cache.get(KEY_CACHE)
-        except Exception as e:
-            return str(e)
+    except Exception as e:
+        return str(e)
