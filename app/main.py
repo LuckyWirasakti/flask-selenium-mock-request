b5eaa2538670aacb2fb3ea09f631249c8f4a6fcb
Lucky Wirasakti <lucky.wirasakti@gmail.com>
2021-10-16 07:25:18 +0700

    update

--- app/main.py
+++ app/main.py
@@ -17,17 +17,14 @@ def root():
 def handler():
     options = webdriver.ChromeOptions()
     options.binary_location = GOOGLE_CHROME_PATH
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--remote-debugging-port=9222')
     options.add_argument(
         'user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/84.0.4147.125 Safari/537.36')
+        'Chrome/84.0.4147.125 Safari/537.36'
+    )
     wb = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
-    wb.get(request.args.get('url'))
     try:
+        wb.get(request.args.get('url'))
         return jsonify(loads(loads(wb.find_element(by=By.TAG_NAME, value="pre").text)))
     except Exception as e:
         return str(e)
